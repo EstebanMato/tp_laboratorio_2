81964fe4581d0726848acd407085eab398394818
esteban mato <estebanemato@gmail.com>
2021-06-05 21:07:06 -0300

    Subido RECUPERATORIO TP2

---
+++ RecuperatoriosTP/TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        /// <summary>
+        /// Constructor publico que recibe como parametro la marca, el chasis y el color
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Imprime los datos del ciclomotor 
+        /// </summary>
+        /// <returns>String con todos los datos</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.Append(base.Mostrar());
+            sb.AppendLine("\n---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP2/Entidades/Suv.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        /// <summary>
+        /// Constructor publico que recibe la marca, el chasis y el color
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        /// <summary>
+        /// Imprime los datos del suv
+        /// </summary>
+        /// <returns>String con todos los datos</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.Append(base.Mostrar());
+            sb.AppendLine("\n---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
